b606bc133d8b67475385f51fd38b5c7dd7f1db3e
mikhawa <michaeljpitz@gmail.com>
2024-09-17 16:59:29 +0200

    section presque ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Phrase;
 use App\Entity\Section;
 use App\Repository\SectionRepository;
-
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,6 +17,20 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'menus' => $sectionRepository->findAll(),
+        ]);
+    }
+    #[Route('/section/{slug}', name: 'section', methods: ['GET'])]
+    public function section(SectionRepository $sectionRepository, string $slug,EntityManagerInterface $em): Response
+    {
+        $section = $sectionRepository->findOneBy(['slug_title' => $slug]);
+        if (!$section) {
+            throw $this->createNotFoundException('The section does not exist');
+        }
+        //$phrases = $em->getRepository(Phrase::class)->findBy(['sections' => $section->getId()]);
+        return $this->render('main/section.html.twig', [
+            'menus' => $sectionRepository->findAll(),
+            'section' => $section,
+            //'phrases' => $phrases,
         ]);
     }
 }
