2fad31b8d3bd6b12a952b37edd6bc7d433e273b8
mikhawa <michaeljpitz@gmail.com>
2024-09-17 15:59:28 +0200

    template ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Section;
+use App\Repository\SectionRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(SectionRepository $sectionRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'menus' => $sectionRepository->findAll(),
         ]);
     }
 }
