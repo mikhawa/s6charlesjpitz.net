efd928a650b36e91d2ff755105ac0272f0478d15
mikhawa <michaeljpitz@gmail.com>
2024-09-19 15:11:56 +0200

    section

--- src/Controller/Admin/PhraseCrudController.php
+++ src/Controller/Admin/PhraseCrudController.php
@@ -3,10 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Phrase;
+use App\Entity\Section;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
@@ -18,13 +17,14 @@ class PhraseCrudController extends AbstractCrudController
     }
 
 
+
     public function configureFields(string $pageName): iterable
     {
         return [
            // IdField::new('id'),
             TextField::new('title'),
             BooleanField::new('published'),
-            AssociationField::new('sections'),
+            AssociationField::new('sections', 'Section')->hideOnIndex(),
         ];
     }
 
